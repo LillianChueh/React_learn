a6e6387d44344ddac2a40f93b5333b039e0aa16b
LillianChueh <yvone5116@gmail.com>
2022-02-08 00:04:28 +0800

    axios

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     onSearchSubmit(term){
-        console.log(term);
+        axios.get("https://api.unsplash.com/search/photos", {
+            params:{ query: term },
+            headers: {
+            Authorization:
+              "Client-ID 9TRepZmy5ylbq9CuAS_DpbmrHjS9KMt5y98gGeEloY4",
+          },
+        });
     };
 
     render(){
